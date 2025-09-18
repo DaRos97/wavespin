dec43f48a6448bf6263cf17f87bb87d8f8a83463
DaRos97 <dario.rossim@gmail.com>
2025-09-18 18:33:53 +0200

    maybe there with 1->3 magnons

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,164 +1,137 @@
 import numpy as np
 
 # General
-def theta_they(*pars):
+def theta_fm(*pars):
     J,S,lam,H = pars
     Hc = 4*J*S*(1-lam)
     if abs(H/Hc)<1:
         return np.arcsin(H/Hc)
     else:
         return np.pi/2
-def theta_my(*pars):
+def theta_afm(*pars):
     J,S,D,H = pars
     Hc = 4*J*S*(1-D)
     if abs(H/Hc)<1:
         return np.arccos(H/Hc)
     else:
         return 0
-# Numeric
-def gamma(k,*pars):
+def p_afm(*pars):
+    J,S,D,H = pars
+    th = theta_afm(*pars)
+    return (-J*S**2*(np.cos(th)**2+D*np.sin(th)**2),
+            J*S**2,
+            -J*S**2*(np.sin(th)**2+D*np.cos(th)**2),
+            -4*J*S**2*(1-D)*np.cos(th)**2
+            )
+def p_fm(*pars):
+    J,S,lam,H = pars
+    ph = theta_fm(*pars)
+    return (-J*S**2*(np.sin(ph)**2+lam*np.cos(ph)**2),
+            -J*S**2,
+            -J*S**2*(np.cos(ph)**2+lam*np.cos(ph)**2),
+            -4*J*S**2*(1-lam)*np.sin(ph)**2
+            )
+def gamma(k):
     if len(k.shape)==1:
         return 1/2*(np.cos(k[0]) + np.cos(k[1]))
     elif len(k.shape)==2:
         return 1/2*(np.cos(k[:,0]) + np.cos(k[:,1]))
+    elif len(k.shape)==3:
+        return 1/2*(np.cos(k[:,:,0]) + np.cos(k[:,:,1]))
     else:
         print("error in gamma")
         exit()
-def epsilon_they(k,gk,*pars):
-    J,S,lam,H = pars
-    th = theta_they(*pars)
-    return 4*J*S*np.sqrt((1-gk)*(1-gk*(lam*np.cos(th)**2+np.sin(th)**2)))
-def epsilon_my(k,gk,*pars):
-    J,S,D,H = pars
-    th = theta_my(*pars)
-    return 4*J*S*np.sqrt((1+gk)*(1-gk*(D*np.sin(th)**2+np.cos(th)**2)))
-def Ak_they(k,gk,*pars):
-    J,S,lam,H = pars
-    th = theta_they(*pars)
-    return 4*J*S*(1-1/2*gk*(1+lam*np.cos(th)**2+np.sin(th)**2))
-def Ak_my(k,gk,*pars):
-    J,S,D,H = pars
-    th = theta_my(*pars)
-    return 4*J*S*(1+1/2*gk*np.sin(th)**2*(1-D))
-def uk_(k,gk,Ak_,epsilon_,*pars):
-    ak = Ak_(k,gk,*pars)
-    ek = epsilon_(k,gk,*pars)
-    result = np.zeros_like(ek, dtype=float)
-    mask = ek != 0
-    result[mask] = np.sqrt((ak[mask] + ek[mask]) / (2 * ek[mask]))
-    return result
-def vk_(k,gk,Ak_,epsilon_,*pars):
-    ak = Ak_(k,gk,*pars)
-    ek = epsilon_(k,gk,*pars)
+def N11(gk,ps,*pars):
+    S = pars[1]
+    return 2 * ( gk*(ps[0]+ps[1])/S - 2*ps[2]/S - ps[3]/2/S )
+def N12(gk,ps,*pars):
+    S = pars[1]
+    return 2*gk*(ps[0]-ps[1])/S
+def epsilon(gk,ps,*pars):
+    return np.sqrt(N11(gk,ps,*pars)**2 - N12(gk,ps,*pars)**2)
+def bogoliubovFunctions(gk,eps,ps,*pars):
+    Ak = N11(gk,ps,*pars)
+    uk = np.zeros_like(eps, dtype=float)
+    mask_u = eps != 0
+    uk[mask_u] = np.sqrt((Ak[mask_u] + eps[mask_u]) / (2 * eps[mask_u]))
+    #
+    vk = np.zeros_like(eps, dtype=float)
     absGk = np.absolute(gk)
-    result = np.zeros_like(ek, dtype=float)
-    mask = (ek != 0) & (absGk != 0) & (ak-ek>0)
-    result[mask] = -np.sqrt((ak[mask] - ek[mask]) / (2 * ek[mask]))*gk[mask]/absGk[mask]
-    return result
+    mask_v = (eps != 0) & (absGk != 0) & (Ak-eps>0)
+    vk[mask_v] = -np.sqrt((Ak[mask_v] - eps[mask_v]) / (2 * eps[mask_v]))*gk[mask_v]/absGk[mask_v]
+    return uk, vk
+# 1 -> 2 decay
 def Gamma3_(k,eps_k,K_grid,eps_q,eta,Ak_,epsilon_,th,*pars):
     L = int(np.sqrt(K_grid.shape[0]))
-    gkmq = gamma(k-K_grid,*pars)
-    eps_kmq = epsilon_(k-K_grid,gkmq,*pars)             # shape (L^2) -> can make this faster
+    gkmq = gamma(k-K_grid)
+    eps_kmq = epsilon_(gkmq,*pars)             # shape (L^2) -> can make this faster
     arg = eps_k - (eps_q + eps_kmq)         # shape (L^2)
     # evaluate delta and sum
     delta_vals = lorentz(arg, eta)            # shape (L^2)
     V_k_q = V3(k,K_grid,Ak_,epsilon_,th,*pars)              # shape (L^2)
     S = np.pi/2*np.sum(V_k_q**2 * delta_vals)
     return S
+def V3(k,q,Ak_,epsilon_,th,*pars):
+    J,S,_,H = pars
+    factor = np.cos(th) if Ak_==Ak_fm else np.sin(th)
+    #
+    gk = gamma(k)
+    uk = uk_(gk,Ak_,epsilon_,*pars)
+    vk = vk_(gk,Ak_,epsilon_,*pars)
+    #
+    gq = gamma(q)
+    uq = uk_(gq,Ak_,epsilon_,*pars)
+    vq = vk_(gq,Ak_,epsilon_,*pars)
+    #
+    gkmq = gamma(k-q)
+    ukmq = uk_(gkmq,Ak_,epsilon_,*pars)
+    vkmq = vk_(gkmq,Ak_,epsilon_,*pars)
+    return H*factor/np.sqrt(2*S) * (  gk*(uk+vk)*(uq*vkmq+vq*ukmq)
+                                        + gq*(uq+vq)*(uk*ukmq+vk*vkmq)
+                                        + gkmq*(ukmq+vkmq)*(uk*uq+vk*vq)   )
+# 1 -> 3 decay
+from time import time as ttt
+def Gamma4a_(k,K_grid,gk,g_grid,gppq,eps_k,eps_grid,eta,ps,*pars):
+    gkmqmp = gamma(k-K_grid[:,None,:]-K_grid[None,:,:])
+    eps_kmqmp = epsilon(gkmqmp,ps,*pars)
+    # Compute delta
+    arg = eps_k - (eps_grid[:,None] + eps_grid[None,:] + eps_kmqmp)   # shape (L^2,L^2)
+    delta_vals = lorentz(arg, eta)              # shape (L^2,L^2)
+    # Computation of U_k(q,p)
+    f1 = -(ps[0]+ps[1])/(8*pars[1]**2)
+    f2 = (ps[1]-ps[0])/(8*pars[1]**2)
+    f3 = ps[2]/pars[1]**2
+    uk,vk = bogoliubovFunctions(gk,eps_k,ps,*pars)
+    t1 = ttt()
+    uq,vq = bogoliubovFunctions(g_grid,eps_grid,ps,*pars)
+    uq = uq[:,None]
+    vq = vq[:,None]
+    up,vp = bogoliubovFunctions(g_grid,eps_grid,ps,*pars)
+    up = up[None,:]
+    vp = vp[None,:]
+    ukmqmp,vkmqmp = bogoliubovFunctions(gkmqmp,eps_kmqmp,ps,*pars)
+    gkm = gamma(k-K_grid)
+    gq = g_grid[:,None]
+    gp = g_grid[None,:]
+    gkmq = gkm[:,None]
+    gkmp = gkm[None,:]
+    uuv = uq*up*vkmqmp + uq*vp*ukmqmp + vq*up*ukmqmp
+    vvu = uq*vp*vkmqmp + vq*vp*ukmqmp + vq*up*vkmqmp
+    guuv = gq*uq*up*vkmqmp + gkmqmp*uq*vp*ukmqmp + gp*vq*up*ukmqmp
+    gvvu = gp*uq*vp*vkmqmp + gq*vq*vp*ukmqmp + gkmqmp*vq*up*vkmqmp
+    U_k_qp = (  gk * ( (f1*uk+f2*vk)*uuv + (f1*vk+f2*uk)*vvu)
+                + (f1*uk+f2*vk)*gvvu + (f1*vk+f2*uk)*guuv
+                + gkmq * (uk*uq + vk*vq) * (up*vkmqmp + vp*ukmqmp)
+                + gkmp * (uk*up + vk*vp) * (uq*vkmqmp + vq*ukmqmp)
+                + gppq * (uk*ukmqmp + vk*vkmqmp) * (up*vq + vp*uq)
+                )
+    result = np.pi/6*np.sum(U_k_qp**2 * delta_vals)
+    return result
+
 def gauss(x,eta):
     return np.exp(-0.5*(x/eta)**2) / (np.sqrt(2*np.pi)*eta)
 def lorentz(x,eta):
     return (1.0/np.pi) * (eta / (x**2 + eta**2))
-def V3(k,q,Ak_,epsilon_,th,*pars):
-    J,S,_,H = pars
-    factor = np.cos(th) if Ak_==Ak_they else np.sin(th)
-    #
-    gk = gamma(k,*pars)
-    uk = uk_(k,gk,Ak_,epsilon_,*pars)
-    vk = vk_(k,gk,Ak_,epsilon_,*pars)
-    #
-    gq = gamma(q,*pars)
-    uq = uk_(q,gq,Ak_,epsilon_,*pars)
-    vq = vk_(q,gq,Ak_,epsilon_,*pars)
-    #
-    gkmq = gamma(k-q,*pars)
-    ukmq = uk_(k-q,gkmq,Ak_,epsilon_,*pars)
-    vkmq = vk_(k-q,gkmq,Ak_,epsilon_,*pars)
-    return H*factor/np.sqrt(2*S) * (  gk*(uk+vk)*(uq*vkmq+vq*ukmq)
-                                        + gq*(uq+vq)*(uk*ukmq+vk*vkmq)
-                                        + gkmq*(ukmq+vkmq)*(uk*uq+vk*vq)   )
-
-from tqdm import tqdm
-def Gamma4a_(k,eps_k,K_grid,eps_p,eta,Ak_,epsilon_,theta_,*pars):
-    L = int(np.sqrt(K_grid.shape[0]))
-    result = 0
-    for ind_q in tqdm(range(L**2)):
-        q = K_grid[ind_q]
-        eps_q = eps_p[ind_q]
-        gkmqmp = gamma(k-q-K_grid,*pars)
-        eps_kmqmp = epsilon_(k-q-K_grid,gkmqmp,*pars)           # shape (L^2) -> can make this faster
-        arg = eps_k - (eps_q + eps_p + eps_kmqmp)       # shape (L^2)
-        delta_vals = lorentz(arg, eta)                  # shape (L^2)
-        U_k_qp = U4a(k,q,K_grid,Ak_,epsilon_,theta_,*pars)                  # shape (L^2)
-        result += np.pi/6*np.sum(U_k_qp**2 * delta_vals)
-    return result
-def U4a(k,q,p,Ak_,epsilon_,theta_,*pars):
-    J,S,D,H = pars
-    th = theta_(*pars)
-    if Ak_==Ak_they:
-        f1 = np.sin(th)**2+1+D*np.cos(th)**2
-        f2 = f1
-        f3 = -4*(np.cos(th)**2+D*np.sin(th)**2)
-    else:
-        print("Not implemented")
-        exit()
-    #
-    gk = gamma(k,*pars)
-    uk = uk_(k,gk,Ak_,epsilon_,*pars)
-    vk = vk_(k,gk,Ak_,epsilon_,*pars)
-    #
-    gq = gamma(q,*pars)
-    uq = uk_(q,gq,Ak_,epsilon_,*pars)
-    vq = vk_(q,gq,Ak_,epsilon_,*pars)
-    #
-    gp = gamma(p,*pars)
-    up = uk_(p,gp,Ak_,epsilon_,*pars)
-    vp = vk_(p,gp,Ak_,epsilon_,*pars)
-    #
-    gkmqmp = gamma(k-q-p,*pars)
-    ukmqmp = uk_(k-q-p,gkmqmp,Ak_,epsilon_,*pars)
-    vkmqmp = vk_(k-q-p,gkmqmp,Ak_,epsilon_,*pars)
-    #
-    gkmp = gamma(k-p,*pars)
-    #
-    gppq = gamma(p+q,*pars)
-    return 3/2*J*(
-        f1*( (gk+gp)*(uq*vkmqmp+vq*ukmqmp)*(vk*up+uk*vp) + gq*(vk*vkmqmp+uk*ukmqmp)*(vq*vp+uq*up) + gkmqmp*(vp*vkmqmp+up*ukmqmp)*(vk*vq+uk*uq) ) +
-        f2*( gk*(vk*uq+uk*vq)*(vp*vkmqmp+up*ukmqmp) + gq*(vk*vp+uk*uq)*(uq*vkmqmp+vq*ukmqmp) + (gp+gkmqmp)*(vp*ukmqmp+up*vkmqmp)*(vk*vq+uk*uq) ) +
-        f3*( gppq*(vk*vkmqmp+uk*ukmqmp)*(uq*vp+vq*up) + gkmp*(uq*vkmqmp+vq*ukmqmp)*(vk*vp+uk*up) )
-    )
-
-def _getQs3(k,k_grid,shellFactor=1e-3,*pars):
-    """ Compute which q values are on shell with k. """
-    ek = epsilon(k,*pars)
-    Eq = epsilon(k_grid,*pars)
-    Ekmq = epsilon(k-k_grid,*pars)
-    inds = np.where(abs(ek-Eq-Ekmq)<shellFactor)
-    if len(inds)>0:
-        return k_grid[ inds[0] ]
-    else:
-        return np.array([])
-
-def _getQs4a(k,k_grid,shellFactor=1e-3,*pars):
-    """ Compute which q values are on shell with k. """
-    ek = epsilon(k,*pars)
-    Eq = epsilon(k_grid,*pars)
-    Ekmq = epsilon(k-k_grid,*pars)
-    inds = np.where(abs(ek-Eq-Ekmq)<shellFactor)
-    if len(inds)>0:
-        return k_grid[ inds[0] ]
-    else:
-        return np.array([])
 
 # Analytic
 def c_(*pars):
--- scripts/magnonLifetime.py
+++ scripts/magnonLifetime.py
@@ -9,37 +9,9 @@ import sys
 
 save = True
 
-J = 1
-S = 1/2
-D_list = [0.0,]
-fH_list = [0.1,0.5,0.9]
-prec0 = 1e-8
-if 0:       #Decide size and eta from input line
-    L_list = [20,50,70,100,150,200,300,400]
-    eta_list = [0.1,0.05,0.01,0.005,0.001,0.0005]
-
-    ind = int(sys.argv[1])
-    il = ind//len(eta_list)
-    ie = ind%len(eta_list)
-    L = L_list[il]
-    eta = eta_list[ie]
-else:
-    L = 700
-    eta = 0.005
-
-print("Parameters: J=%d"%J)
-print("fH values: ",fH_list)
-print("D values: ",D_list)
-print("BZ linear size: ",L)
-print("Gaussian broadening: ",eta)
-
-k_list = np.linspace(0,np.pi,L,endpoint=False)
-KX,KY = np.meshgrid(k_list,k_list,indexing='ij')
-K_grid = np.column_stack(([KX.ravel(),KY.ravel()]))
-
 if 0:   #Plot curvature at different fields
-    D_list = [0.,0.3,0.7,0.9]
-    fH_list = [0,]#0.5,0.9]
+    D_list = [0.]#,0.3,0.7,0.9]
+    fH_list = [0,0.5,0.9,1]
     Ngm = 500
     Nmx = int(Ngm/np.sqrt(2))
     Nk = Ngm+Nmx
@@ -54,8 +26,8 @@ if 0:   #Plot curvature at different fields
         Hc = 4*S*J*(1-lam)
         for fH in fH_list:
             parameters = (J,S,lam,fH*Hc)
-            gk = fs.gamma(k_to_compute,*parameters)
-            ek = fs.epsilon_they(k_to_compute,gk,*parameters)
+            gk = fs.gamma(k_to_compute)
+            ek = fs.epsilon_they(gk,*parameters)
             ax.plot(np.arange(Nk),ek,label=r"H/Hc=%.2f, $\lambda$=%.2f"%(fH,lam))
     ax.axvline(Ngm,color='r',ls='--')
     ax.set_xticks([0,Ngm,Nk-1],[r'$\Gamma$',r'$M$',r'$X$'],size=20)
@@ -68,8 +40,8 @@ if 0:   #Plot curvature at different fields
         Hc = 4*S*J*(1-D)
         for fH in fH_list:
             parameters = (J,S,D,fH*Hc)
-            gk = fs.gamma(k_to_compute,*parameters)
-            ek = fs.epsilon_my(k_to_compute,gk,*parameters)
+            gk = fs.gamma(k_to_compute)
+            ek = fs.epsilon_my(gk,*parameters)
             ax.plot(np.arange(Nk),ek,label=r"H/Hc=%.2f, D=%.2f"%(fH,D))
     ax.axvline(Ngm,color='r',ls='--')
     ax.set_xticks([0,Ngm,Nk-1],[r'$\Gamma$',r'$M$',r'$X$'],size=20)
@@ -78,63 +50,99 @@ if 0:   #Plot curvature at different fields
     ax.set_ylim(0,4)
     plt.show()
     exit()
-
-if 1:
-    """ Compute decay rate of 1->2 magnon processes """
+if 0:
+    """ Compute phase space of 1->2 magnon process """
+    H = 0.1
+    if 0:
+        lam = 0.5
+        parameters = (J,S,lam,H)
+        epsilon_ = fs.epsilon_they
+    else:
+        D = 0.1
+        parameters = (J,S,D,H)
+        epsilon_ = fs.epsilon_my
+    res = np.zeros(L**2)
+    gq = fs.gamma(K_grid)
+    eq = epsilon_(gq,*parameters)
+    for ik in tqdm(range(L**2)):
+        k = K_grid[ik]
+        ek = eq[ik]
+        gkmq = fs.gamma(k-K_grid)
+        ekmq = epsilon_(gkmq,*parameters)
+        spread = fs.lorentz(ek - (eq + ekmq),eta)
+        vals = spread[spread>1/np.pi/eta-0.1]
+        res[ik] = np.sign(len(vals)-2)
+    res[res<0.5] = np.nan
+    fig = plt.figure(figsize=(15,12))
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(KX,KY,res.reshape(L,L),cmap='plasma')
+    plt.show()
+    exit()
+if 0:
+    """ Compute decay rate of 1->2 magnon process """
+    J = 1
+    S = 1/2
+    D_list = [-0.5,]
+    fH_list = [0.3,0.5,0.7,0.9]
+    Ham = 'FM'      #FM or AFM
     Ngm = 100
+    L = 200
+    eta = 0.001
+    k_list = np.linspace(0,np.pi,L,endpoint=False)
+    KX,KY = np.meshgrid(k_list,k_list,indexing='ij')
+    K_grid = np.column_stack(([KX.ravel(),KY.ravel()]))
+    print("Parameters: J=%d"%J)
+    print("fH values: ",fH_list)
+    print("D values: ",D_list)
+    print("BZ linear size: ",L)
+    print("Gaussian broadening: ",eta)
     Nmx = int(Ngm/np.sqrt(2))
     Nk = Ngm+Nmx
     k_gm = np.column_stack((np.linspace(0,np.pi,Ngm,endpoint=False),np.linspace(0,np.pi,Ngm,endpoint=False)))
     k_mx = np.column_stack((np.ones(Nmx)*np.pi,np.linspace(np.pi,0,Nmx)))
     k_to_compute = np.append(k_gm,k_mx,axis=0)
-    Gamma3_my = np.zeros((len(D_list),len(fH_list),Nk))
-    Gamma3_they = np.zeros((len(D_list),len(fH_list),Nk))
+    Gamma3 = np.zeros((len(D_list),len(fH_list),Nk))
     for idd,D in enumerate(D_list):
         for ih,fH in enumerate(fH_list):
-            filename = "Data/1to2decay_J%.2f_D%.2f_H%.2f_Nk%d_L%d_eta%.7f.npz"%(J,D,fH,Ngm,L,eta)
+            filename = "Data/1to2decay_%s_J%.2f_D%.2f_H%.2f_Nk%d_L%d_eta%.7f.npy"%(Ham,J,D,fH,Ngm,L,eta)
             if Path(filename).is_file():
-                print("Loading from file")
-                Gamma3_my = np.load(filename)['my']
-                Gamma3_they = np.load(filename)['they']
+                print("Loading from file D=%.2f, fH=%.2f, %s"%(D,fH,Ham))
+                Gamma3 = np.load(filename)
             else:
-                Hc = 4*J*S*(1-D)
-                parameters_my = (J,S,D,fH*Hc)
-                theta_my = fs.theta_my(*parameters_my)
-                gk_my = fs.gamma(k_to_compute,*parameters_my)
-                ek_my = fs.epsilon_my(k_to_compute,gk_my,*parameters_my)
-                gq_my = fs.gamma(K_grid,*parameters_my)
-                eq_my = fs.epsilon_my(K_grid,gq_my,*parameters_my)
-                #
-                lam = -D
-                Hc = 4*J*S*(1-lam)
-                parameters_they = (J,S,lam,fH*Hc)
-                theta_they = fs.theta_my(*parameters_they)
-                gk_they = fs.gamma(k_to_compute,*parameters_they)
-                ek_they = fs.epsilon_they(k_to_compute,gk_they,*parameters_they)
-                gq_they = fs.gamma(K_grid,*parameters_they)
-                eq_they = fs.epsilon_they(K_grid,gq_they,*parameters_they)
+                if Ham=='AFM':
+                    Hc = 4*J*S*(1-D)
+                    parameters = (J,S,D,fH*Hc)
+                    theta = fs.theta_afm(*parameters)
+                    epsilon_ = fs.epsilon_afm
+                    Ak_ = fs.Ak_afm
+                else:
+                    lam = -D
+                    Hc = 4*J*S*(1-lam)
+                    parameters = (J,S,lam,fH*Hc)
+                    theta = fs.theta_fm(*parameters)
+                    epsilon_ = fs.epsilon_fm
+                    Ak_ = fs.Ak_fm
+                gk = fs.gamma(k_to_compute)
+                ek = epsilon_(gk,*parameters)
+                gq = fs.gamma(K_grid)
+                eq = epsilon_(gq,*parameters)
                 for ik in tqdm(range(Nk)):
                     k = k_to_compute[ik]
-                    if not np.linalg.norm(k-np.array([np.pi,np.pi]))<prec0:     #skip k=0 -> gapless -> problems
-                        Gamma3_my[idd,ih,ik] = fs.Gamma3_(k,ek_my[ik],K_grid,eq_my,eta,fs.Ak_my,fs.epsilon_my,theta_my,*parameters_my)/L**2
-                    if not np.linalg.norm(k)<prec0:     #skip k=0 -> gapless -> problems
-                        Gamma3_they[idd,ih,ik] = fs.Gamma3_(k,ek_they[ik],K_grid,eq_they,eta,fs.Ak_they,fs.epsilon_they,theta_they,*parameters_they)/L**2
+                    Gamma3[idd,ih,ik] = fs.Gamma3_(k,ek[ik],K_grid,eq,eta,Ak_,epsilon_,theta,*parameters)/L**2
             if save:
-                np.savez(filename,my=Gamma3_my,they=Gamma3_they)
+                np.save(filename,Gamma3)
 
     fig = plt.figure(figsize=(15,7))
     ax = fig.add_subplot()
     x = np.arange(Nk)
-    colors_my = ['gray','orange','lime','aqua']
-    colors_they = ['k','r','g','b']
+    colors = ['gray','orange','lime','aqua']
     for idd,D in enumerate(D_list):
         for ih,fH in enumerate(fH_list):
-            ax.plot(x,abs(Gamma3_my[idd,ih]),label='AFM-staggered D=%.2f, H/Hc=%.2f'%(D,fH))
-    if 0:
-        for idd,D in enumerate(D_list):
-            lam = -D
-            for ih,fH in enumerate(fH_list):
-                ax.plot(x,abs(Gamma3_they[idd,ih]),label=r'FM-uniform $\lambda$=%.2f, H/Hc=%.2f'%(lam,fH))
+            if Ham=='FM':
+                txt, v = (r'$\lambda$',-D)
+            else:
+                txt, v = ('D',D)
+            ax.plot(x,abs(Gamma3[idd,ih]),label='%s, %s=%.2f, H/Hc=%.2f'%(Ham,txt,v,fH))
     ax.axvline(Ngm,color='r',ls='--')
     ax.set_xlim(0,Nk)
     ax.set_xticks([0,Ngm,Nk-1],[r'$\Gamma$',r'$M$',r'$X$'],size=20)
@@ -142,7 +150,7 @@ if 1:
     ax.set_title(r"1->2 decay process, L=%d, eta=%.7f"%(L,eta),size=30)
 #        ax.set_ylim(0,0.7)
     ax.legend(fontsize=20)
-    figname = "Figures/1to2decay_J%.2f_Nk%d_L%d_eta%.5f_D"%(J,Ngm,L,eta)
+    figname = "Figures/1to2decay_%s_J%.2f_Nk%d_L%d_eta%.5f_D"%(Ham,J,Ngm,L,eta)
     for D in D_list:
         figname += "{:.3f}".format(D)
         if not D==D_list[-1]:
@@ -158,63 +166,133 @@ if 1:
     if save:
         fig.savefig(figname)
     plt.show()
-
+    exit()
 if 0:
-    """ Compute decay rate of 1->2 magnon processes """
-    Nk = 30
-    k_to_compute_they = np.column_stack((np.linspace(0,np.pi,Nk,endpoint=False),np.linspace(0,np.pi,Nk,endpoint=False)))
-    k_to_compute_my = k_to_compute_they#np.column_stack((np.linspace(0,np.pi,Ngm,endpoint=False),np.linspace(0,np.pi,Ngm,endpoint=False)))
-    filename = "Data/1to3decay_J%.2f_H%.2f_Nk%d_L%d_eta%.5f_D"%(J,H,Nk,L,eta)
-    for D in D_list:
-        filename += "{:.3f}".format(D)
-        if not D==D_list[-1]:
-            filename += '_'
-        else:
-            filename += '.npz'
-    if not Path(filename).is_file():
-        """ Compute decay rate of 1->2 magnon processes """
-        Gamma4a_my = np.zeros((len(H_list),Nk))
-        Gamma4a_they = np.zeros((len(H_list),Nk))
-        for ind_d,D in enumerate(D_list):
-            parameters = (J,S,D,H)
-            gk_they = fs.gamma(k_to_compute_they,*parameters)
-            ek_they = fs.epsilon_they(k_to_compute_they,gk_they,*parameters)
-            gq_they = fs.gamma(K_grid_my,*parameters)
-            eq_they = fs.epsilon_they(K_grid_they,gq_they,*parameters)
-            for ind in tqdm(range(Nk)):
-                k_they = k_to_compute_they[ind]
-                if not np.linalg.norm(k_they)<prec0:     #skip k=0 -> gapless -> problems
-                    Gamma4a_they[ind_d,ind] = fs.Gamma4a_(k_they,ek_they[ind],K_grid_they,eq_they,eta,fs.Ak_they,fs.epsilon_they,fs.theta_they,*parameters)/L**4
-        if save:
-            np.savez(filename,my=Gamma4a_my,they=Gamma4a_they)
+    """ Compute phase space of 1->3 magnon process """
+    H = 0.
+    if 1:
+        lam = 0.5
+        parameters = (J,S,lam,H)
+        epsilon_ = fs.epsilon_they
     else:
-        print("Loading from file")
-        Gamma4a_my = np.load(filename)['my']
-        Gamma4a_they = np.load(filename)['they']
+        D = 0.1
+        parameters = (J,S,D,H)
+        epsilon_ = fs.epsilon_my
+    res = np.zeros(L**2)
+    gq = fs.gamma(K_grid)
+    ep = epsilon_(gq,*parameters)
+    for ik in tqdm(range(L**2)):
+        k = K_grid[ik]
+        ek = ep[ik]
+        for iq in range(L**2):
+            q = K_grid[iq]
+            eq = ep[iq]
+            gkmqmp = fs.gamma(k-q-K_grid)
+            ekmqmp = epsilon_(gkmqmp,*parameters)
+            spread = fs.lorentz(ek - (eq + ep + ekmqmp),eta)
+            vals = spread[spread>1/np.pi/eta-0.1]
+            res[ik] += len(vals)
+#    res[res<0.5] = np.nan
+    fig = plt.figure(figsize=(15,12))
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(KX,KY,np.log(res.reshape(L,L)),cmap='plasma')
+    plt.show()
+    exit()
+if 1:
+    """ Compute decay rate of 1->3 magnon process """
+    J = 1
+    S = 1/2
+    D_list = [-0.3,]
+    fH_list = [0.]
+    Ham = 'AFM'      #FM or AFM
+    Nk = 31
+    ind = int(sys.argv[1])
+    L_list = [50,70,100,120,150]
+    eta_list = [1e-4,1e-5,1e-6,1e-7]
+    L = L_list[ind//len(eta_list)]
+    eta = eta_list[ind%len(eta_list)]
+    k_list = np.linspace(0,np.pi,L,endpoint=False)
+    KX,KY = np.meshgrid(k_list,k_list,indexing='ij')
+    K_grid = np.column_stack(([KX.ravel(),KY.ravel()]))
+    print("Parameters: J=%d"%J)
+    print("fH values: ",fH_list)
+    print("D values: ",D_list)
+    print("BZ linear size: ",L)
+    print("Gaussian broadening: ",eta)
+    print("Points in G-M cut: ",Nk)
+    vals = np.linspace(0*np.pi,np.pi,Nk,endpoint=1)#True)
+    k_to_compute = np.column_stack((vals,vals))
+    Gamma4 = np.zeros((len(D_list),len(fH_list),Nk))
+    for idd,D in enumerate(D_list):
+        for ih,fH in enumerate(fH_list):
+            filename = "Data/1to3decay_%s_J%.2f_D%.2f_H%.2f_Nk%d_L%d_eta%.7f.npy"%(Ham,J,D,fH,Nk,L,eta)
+            if Path(filename).is_file():
+                print("Loading from file D=%.2f, fH=%.2f, %s"%(D,fH,Ham))
+                Gamma4 = np.load(filename)
+            else:
+                if Ham=='AFM':
+                    Hc = 4*J*S*(1-D)
+                    parameters = (J,S,D,fH*Hc)
+                    ps = fs.p_afm(*parameters)
+                else:
+                    lam = -D
+                    Hc = 4*J*S*(1-lam)
+                    parameters = (J,S,lam,fH*Hc)
+                    ps = fs.p_fm(*parameters)
+                gk = fs.gamma(k_to_compute)
+                eps_k = fs.epsilon(gk,ps,*parameters)
+                #
+                g_grid = fs.gamma(K_grid)
+                gq = g_grid[:,None]
+                gp = g_grid[None,:]
+                gppq = fs.gamma(K_grid[:,None] + K_grid[None,:])
+                eps_grid = fs.epsilon(g_grid,ps,*parameters)
+                for ik in tqdm(range(Nk)):      #this maybe can be also parallelized
+                    k = k_to_compute[ik]
+                    if k[0]==np.pi:
+                        continue
+                    Gamma4[idd,ih,ik] = fs.Gamma4a_(k,K_grid,gk[ik],g_grid,gppq,eps_k[ik],eps_grid,eta,ps,*parameters)/L**4
+                    print(k[0]/np.pi)
+                    print(Gamma4[idd,ih,ik])
+                    print('--------------------------')
+            if save:
+                np.save(filename,Gamma4)
 
-    fig = plt.figure(figsize=(10,7))
+    fig = plt.figure(figsize=(15,7))
     ax = fig.add_subplot()
     x = np.arange(Nk)
-    colors_my = ['gray','orange','lime','aqua']
-    colors_they = ['k','r','g']
-    for ind_d,D in enumerate(D_list):
-#        ax.plot(x,abs(Gamma4a_my[ind_d]),color=colors_my[ind_d],label='AFM-staggered H: %.2f'%H)
-        ax.plot(x,abs(Gamma4a_they[ind_d]),marker='^',color=colors_they[ind_d],label='FM-uniform H: %.2f'%H)
+    colors = ['gray','orange','lime','aqua']
+    for idd,D in enumerate(D_list):
+        for ih,fH in enumerate(fH_list):
+            if Ham=='FM':
+                txt, v = (r'$\lambda$',-D)
+            else:
+                txt, v = ('D',D)
+            ax.plot(x,abs(Gamma4[idd,ih]),'k*-',label='%s, %s=%.2f, H/Hc=%.2f'%(Ham,txt,v,fH))
     ax.set_xlim(0,Nk)
-    ax.set_xticks([0,Nk-1],[r'$\Gamma$',r'$M$'],size=20)
+    ax.set_xticks([0,Nk/4,Nk/2,Nk*3/4,Nk-1],[r'$\Gamma$','','','',r'$M$'],size=20)
+    ax.set_xlim(Nk*k_to_compute[0][0]/np.pi,Nk*k_to_compute[-1][0]/np.pi)
     ax.set_ylabel("Decay rate",size=20)
-    ax.set_title("1->3 decay process, L=%d, eta=%.7f"%(L,eta),size=30)
-#    ax.set_ylim(0,0.7)
+    ax.set_title(r"1->3 decay process, L=%d, eta=%.7f"%(L,eta),size=30)
+#        ax.set_ylim(0,0.7)
     ax.legend(fontsize=20)
-    figname = "Figures/1to3decay_J%.2f_D%.2f_Nk%d_L%d_eta%.5f_H"%(J,D,Nk,L,eta)
-    for H in H_list:
-        figname += "{:.3f}".format(H)
-        if not H==H_list[-1]:
+    figname = "Figures/1to3decay_%s_J%.2f_Nk%d_L%d_eta%.5f_D"%(Ham,J,Nk,L,eta)
+    for D in D_list:
+        figname += "{:.3f}".format(D)
+        if not D==D_list[-1]:
+            figname += '_'
+        else:
+            figname += '_H'
+    for fH in fH_list:
+        figname += "{:.2f}".format(fH)
+        if not fH==fH_list[-1]:
             figname += '_'
         else:
             figname += '.png'
-    fig.savefig(figname)
-#    plt.show()
+    if save:
+        fig.savefig(figname)
+    plt.show()
+    exit()
 
 
 

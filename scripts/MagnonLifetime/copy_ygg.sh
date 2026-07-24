96a1ac4ed336f5d983bee38bcbe0171fb81e52b4
DaRos97 <dario.rossim@gmail.com>
2026-07-24 22:21:58 +0200

    Cleaned up unsused files

--- scripts/MagnonLifetime/copy_ygg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#scp rossid@login1.yggdrasil.hpc.unige.ch:~/5_magnonLifetime/Data/* /home/dario/Desktop/git/WaveSpin/scripts/Data/
-scp rossid@login1.yggdrasil.hpc.unige.ch:~/5_magnonLifetime/Figures/* /home/dario/Desktop/git/WaveSpin/scripts/magnonLifetime/Figures/
